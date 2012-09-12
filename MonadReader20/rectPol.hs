1db2f6213d7aee71ea1cd3db12e8207c7c7e8118
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-09-12 14:39:38 +0100

    / ‘MonadReader20/rectPol.hs’

--- MonadReader20/rectPol.hs
+++ MonadReader20/rectPol.hs
@@ -5,6 +5,7 @@ data Point2D = Cartesian2D Double Double
 
 instance Eq Point2D where
   Cartesian2D x y == Polar2D r theta = 
-      x == rx && y == ry
+      distance (x,y) (rx,ry) < 0.01
           where rx=r * cos theta
                 ry=r * sin theta
+                distance (x,y) (rx,ry) = sqrt ((rx-x)*(rx-x) + (ry-y)*(ry-y))
