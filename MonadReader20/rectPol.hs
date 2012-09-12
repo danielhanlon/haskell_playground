5af7c03707f4e9ab0ce4d46ae2e1f9707cf40aa5
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-09-12 13:03:48 +0100

    + ‘MonadReader20/rectPol.hs’

---
+++ MonadReader20/rectPol.hs
@@ -0,0 +1,13 @@
+cartProd (set:sets) = [x:xs | x<-set, xs<-cp]
+                      where cp=cartProd sets
+cartProd [] = [[]]
+
+data Point2D = Cartesian2D Double Double
+             | Polar2D Double Double
+             deriving (Eq,show)
+
+instance Eq Point2D where
+  Cartesian2D x y == Polar2D r theta = 
+      x == rx && y == ry
+          where rx=r * cos theta
+                ry=r * sin theta
