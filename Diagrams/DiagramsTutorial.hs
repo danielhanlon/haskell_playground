78256045c9ee4637249c775ecd29b138bbdbe904
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-10-25 14:36:17 +0100

    / ‘Diagrams/DiagramsTutorial.hs’

--- Diagrams/DiagramsTutorial.hs
+++ Diagrams/DiagramsTutorial.hs
@@ -3,4 +3,9 @@
 import Diagrams.Prelude
 import Diagrams.Backend.Cairo.CmdLine
 
-main = defaultMain (circle 1)
+circle1 = circle 1 # fc blue
+                   # lw 0.05
+                   # lc purple
+                   # dashing [0.2, 0.05] 0
+
+main = defaultMain circle1
