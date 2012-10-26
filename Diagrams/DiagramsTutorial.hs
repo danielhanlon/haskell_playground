8288f1b7d72b1c746095208b75dcf451920164d5
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-10-26 15:10:20 +0100

    / ‘Diagrams/DiagramsTutorial.hs’

--- Diagrams/DiagramsTutorial.hs
+++ Diagrams/DiagramsTutorial.hs
@@ -7,5 +7,6 @@ circle1 = circle 1 # fc blue
                    # lw 0.05
                    # lc purple
                    # dashing [0.2, 0.05] 0
+                   # pad 1.1
 
 main = defaultMain circle1
