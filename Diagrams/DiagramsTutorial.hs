4b89fdd1a5ec50ba928798a11112be6b175b0d87
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-10-27 11:26:25 +0100

    / ‘Diagrams/DiagramsTutorial’ / ‘Diagrams/DiagramsTutorial.hs’

--- Diagrams/DiagramsTutorial.hs
+++ Diagrams/DiagramsTutorial.hs
@@ -10,6 +10,6 @@ import Diagrams.Backend.Cairo.CmdLine
 --                   # pad 1.1
 
 --circle1 = pad 1.1 . dashing [0.2, 0.05] 0 . lc purple . lw 0.05 . fc blue $ circle 1
-circle1 = circle 1 # fc red # lw 0 ||| circle 1 # fc green # lw 0
+circle1 = circle 1 # fc red # lw 0 ||| circle 1 # fc green # lw 0 # showOrigin
 
 main = defaultMain circle1
