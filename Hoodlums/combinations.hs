ff2145f66e6a379c2b95569afcda4abb09e81421
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-11-15 19:28:10 +0000

    / ‘Hoodlums/combinations.hs’

--- Hoodlums/combinations.hs
+++ Hoodlums/combinations.hs
@@ -3,3 +3,12 @@ comb = do
   a <- [1..height-1]
   let b = height-a
   return (a,b)
+
+height h = do
+  total <- [1..h]
+  a <- [1..total-1]
+  let b = total-a
+  return (a,b)
+
+triple h = ()
+--  let (a,b) = take 
