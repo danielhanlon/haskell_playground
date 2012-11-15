0221a868f610aac4a8d7ae9fc492e37ddb16439b
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-11-15 19:20:05 +0000

    / ‘Hoodlums/atkin.hs’

--- Hoodlums/atkin.hs
+++ Hoodlums/atkin.hs
@@ -1,11 +1,11 @@
-atkin = do m <- [1..60]
-   n <- [1..60]
-   guard (mod (poly m n) 60 == k)
-   return $
-      do j <- [0..]
-         let y = n + 60*j
-         return $
-            do i <- [0..]
-               let x = m + 60*i
-               guard (test x y)
-               return (poly x y)
+atkin = do  m <- [1..60]
+            n <- [1..60]
+            guard (mod (poly m n) 60 == k)
+            return $
+              do  j <- [0..]
+                  let y = n + 60*j
+                  return $
+                    do  i <- [0..]
+                        let x = m + 60*i
+                        guard (test x y)
+                        return (poly x y)
