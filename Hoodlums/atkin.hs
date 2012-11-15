ee1b0d5bc8dd4bbc5fb82d940b602eb5f39664c1
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-11-15 19:12:20 +0000

    / ‘Hoodlums/atkin.hs’

--- Hoodlums/atkin.hs
+++ Hoodlums/atkin.hs
@@ -2,7 +2,7 @@ mylist = do
         a <- [1..]
         b <- [1..]
         c <- [1..]
-        guard (A + B = C)
+        guard (A + B == C)
           where A=a*a
                 B=b*b
                 C=c*c
