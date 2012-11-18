8302b9b492958458317a4060a49792158f138293
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-11-18 21:01:59 +0000

    / ‘Hoodlums/temp.hs’

--- Hoodlums/temp.hs
+++ Hoodlums/temp.hs
@@ -1,12 +1,12 @@
 import Data.Char
 
 sequence_ :: [IO a] -> IO ()
-sequence_ [] = return ()
-sequence_ (x:xs) = do x
-                      sequence_ xs
+my_sequence [] = return ()
+my_sequence (x:xs) = do x
+                        my_sequence xs
 
 upperFile :: String -> IO ()
 upperFile s = do
                   file <- readFile s
                   let ls = lines file
-                  sequence_ $ map  (\l -> map toUpper l) ls
+                  my_sequence $ map  (\l -> map toUpper l) ls
