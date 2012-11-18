5ac76d944305bff3bd129d816d738b31f4d00c23
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-11-18 01:15:50 +0000

    / ‘Hoodlums/temp.hs’

--- Hoodlums/temp.hs
+++ Hoodlums/temp.hs
@@ -4,4 +4,4 @@ upperFile :: String -> IO ()
 upperFile s = do
                   file <- readFile s
                   let ls = lines file
-                  map putStrLn $ map (\l -> map toUpper l) ls
+                  map putStrLn ( map  (\l -> map toUpper l) ls)
