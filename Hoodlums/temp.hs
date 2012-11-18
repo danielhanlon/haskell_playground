115c68b616764b98e855cdcba1b980e310e10b85
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-11-18 21:19:47 +0000

    / ‘Hoodlums/temp.hs’

--- Hoodlums/temp.hs
+++ Hoodlums/temp.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import System.IO
 
 my_sequence :: [IO a] -> IO ()
 my_sequence [] = return ()
@@ -9,5 +10,11 @@ upperFile :: String -> IO ()
 upperFile s = do
                   file <- readFile s
                   let ls = lines file
-                  let actions = map  putStrLn ( map (\l -> map toUpper l) ls)
+                  let actions = map putStrLn ( map (\l -> map toUpper l) ls)
                   my_sequence actions
+
+my_readfilei :: FilePath -> IO (Integer -> IO Char)
+my_readfilei name = do h <- openFile name ReadMode
+                       let readi i = do hSeek h AbsoluteSeek i
+                                        hGetChar h
+                       return readi
