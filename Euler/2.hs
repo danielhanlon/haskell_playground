64bb04b0ff96c0c4d300945b9023c68b0ccb6152
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-18 18:43:46 +0000

    / ‘Euler/2.hs’

--- Euler/2.hs
+++ Euler/2.hs
@@ -12,8 +12,8 @@ sumFibUpTo total = sumFib 1
   where
     sumFib 1 = 1 + sumFib 2
     sumFib n  
-      | fib n  < total, fib n + sumFib (n+1)
-      | otherwise, 0
+      | fib n < total = fib n + sumFib (n+1)
+      | otherwise = 0
     fib 1 = 1
     fib 2 = 2
     fib n = fib (n-1) + fib (n-2)
