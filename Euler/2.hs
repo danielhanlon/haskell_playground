d4db9fe2a388dfea40499b5cbb9ea9a9650b876a
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-18 23:59:43 +0000

    / ‘Euler/2.hs’

--- Euler/2.hs
+++ Euler/2.hs
@@ -1,21 +1,26 @@
+
+fib 1 = 1
+fib 2 = 2
+fib n = fib (n-1) + fib (n-2)
+
 fibonacci :: (Enum a, Num a) => [a] 
 fibonacci = [fib n | n<-[1..]]
   where
-    fib 1 = 1
-    fib 2 = 2
-    fib n = fib (n-1) + fib (n-2)
+  fib 1 = 1
+  fib 2 = 2
+  fib n = fib (n-1) + fib (n-2)
 
 even_fib :: [Integer]
 even_fib = [x | x<-fibonacci, x `mod`2 ==0]
 
---fibUpTo :: Integer -> [Integer]
+  --fibUpTo :: Integer -> [Integer]
 --fibUpTo total = sumFib (1,[])
---  where
---    sumFib (1,_) = sumFib (2,[1])
---    sumFib (2,x) = sumFib (3,x++2)
---    sumFib (n,x)  
+  --  where
+  --    sumFib (1,_) = sumFib (2,[1])
+  --    sumFib (2,x) = sumFib (3,x++2)
+  --    sumFib (n,x)  
 --      | n <= total = sumFib(fib n, x++sumFib (n+1))
---      | otherwise = x
---    fib 1 = 1
---    fib 2 = 2
+  --      | otherwise = x
+  --    fib 1 = 1
+  --    fib 2 = 2
 --    fib n = fib (n-1) + fib (n-2)
