fe6b0d7c8909c61af561ed59439802e5373af59d
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-18 19:58:49 +0000

    / ‘Euler/2.hs’

--- Euler/2.hs
+++ Euler/2.hs
@@ -9,9 +9,9 @@ even_fib :: [Integer]
 even_fib = [x | x<-fibonacci, x `mod`2 ==0]
 
 fibUpTo :: Integer -> [Integer]
-fibUpTo total = [] ++ sumFib 1
+fibUpTo total = sumFib 1
   where
-    sumFib 1 = 1 ++ sumFib 2
+    sumFib 1 = [1] ++ sumFib 2
     sumFib n  
       | fib n <= total = fib n ++ sumFib (n+1)
       | otherwise = 0
