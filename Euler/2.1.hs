939bfede97a9dac36a84c6265a9d736c6afca526
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-19 17:15:57 +0000

    / ‘Euler/2.1.hs’

--- Euler/2.1.hs
+++ Euler/2.1.hs
@@ -2,7 +2,7 @@ data FibFrame = FibFrame Int Int
         deriving Eq
 instance Show FibFrame where
   show (FibFrame a b) = show b
-instance (Ord a) => Ord (FibFrame) where
+instance Ord FibFrame where
   (FibFrame a1 b1) <= (FibFrame a2 b2) = b1<=b2
 nextFibFrame :: FibFrame -> FibFrame
 nextFibFrame ( FibFrame a b ) = FibFrame b (a+b)
