fe1afb8cd5801f1988d86f343c9fb17f77145f5f
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-19 16:00:35 +0000

    / ‘Euler/2.hs’

--- Euler/2.hs
+++ Euler/2.hs
@@ -15,10 +15,9 @@ upTo n (x:xs)
 data FibFrame = FibFrame Ord Ord 
 instance Show FibFrame where
   show (FibFrame a b) = show b
-instance Eq FibFrame where
-  (FibFrame a1 b1) == (FibFrame a2 b2) = ( a1==b1 && a2==b2)
 instance Ord FibFrame where
   (FibFrame a1 b1) <= (FibFrame a2 b2) = b1<=b2
+  (FibFrame a1 b1) == (FibFrame a2 b2) = ( a1==b1 && a2==b2)
 nextFibFrame :: FibFrame -> FibFrame
 nextFibFrame ( FibFrame a b ) = FibFrame b (a+b)
 fib = iterate nextFibFrame (FibFrame 1 1)
