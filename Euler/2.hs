84823606ec496d4c7d9503692f17ca53149196bb
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-19 22:41:53 +0000

    / ‘Euler/2.hs’

--- Euler/2.hs
+++ Euler/2.hs
@@ -11,3 +11,9 @@ even_fib = filter even fibonacci
 upTo n (x:xs)
   | x > n = []
   | otherwise= x:(upTo n xs)
+
+memoized_fib :: Int -> Integer
+memoized_fib = (map fib [0 ..] !!)
+  where fib 0 = 0
+        fib 1 = 1
+        fib n = memoized_fib (n-2) + memoized_fib (n-1)
