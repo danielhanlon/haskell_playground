935bc98d4e6348fb52f030f3e9478282964b7fc8
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-20 11:28:22 +0000

    / ‘Euler/2.hs’

--- Euler/2.hs
+++ Euler/2.hs
@@ -40,6 +40,6 @@ primeFactors :: Int -> [Int]
 primeFactors n = addPrimeFactor 2 []
   where
     addPrimeFactor c fs
-      | c > (n/2) = fs
+      | c > floor (n/2) = fs
       | (n `mod` c == 0) && memoizedIsPrime c = addPrimeFactor (c+1) (c:fs)
       | otherwise = addPrimeFactor (c+1) fs
