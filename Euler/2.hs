b26247a19e86b349a29afe99985a71c7633e78d0
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-20 14:32:23 +0000

    / ‘Euler/2.hs’

--- Euler/2.hs
+++ Euler/2.hs
@@ -43,8 +43,8 @@ primeFactors :: Int -> [Int]
 primeFactors n = addPrimeFactor candidates []
   where
     candidates = upToIncluding (floor $ sqrt $ fromIntegral n) primes
+    addPrimeFactor [] fs = fs
     addPrimeFactor (c:cs) fs
-      | c == [] = fs
       | (n `mod` c == 0) = addPrimeFactor cs (c:fs)
       | otherwise = addPrimeFactor cs fs
 --    addPrimeFactor c fs
