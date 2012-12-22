ceb28a8e9475f9ab6adb0a3cb526cd850246b6b4
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-22 22:45:43 +0000

    / ‘Euler/8’ / ‘Euler/8.hs’

--- Euler/8.hs
+++ Euler/8.hs
@@ -77,4 +77,4 @@ isDivisibleBy n (f:fs) = ( n `mod` f == 0 )  && isDivisibleBy n fs
 main = do
   number_string <- getContents
   let n = (read number_string)::Int
-  putStrLn $ show n
+  putStrLn $ show number_string
