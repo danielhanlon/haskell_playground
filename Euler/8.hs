d3a0c1d05f3ace6b032be2ec39543cc594ef43a0
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-22 23:11:15 +0000

    / ‘Euler/8.hs’

--- Euler/8.hs
+++ Euler/8.hs
@@ -82,6 +82,6 @@ prodFiveConsec :: String -> [Int]
 prodFiveConsec s = pfc' s []
   where
     prod :: String -> String -> String -> String -> String -> Int
-    prod a b c d e = a*b*c*d*e
+    prod a b c d e = (read a)*(read b)*(read c)*(read d)*(read e)
     pfc' (a:b:c:d:e:[]) res = (prod a b c d e):res
     pfc' (a:b:c:d:e:f) res = pfc' f (prod a b c d e):res
