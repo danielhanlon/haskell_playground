4cb0d5801b523de2b16515902f4a8fb3c0033a2d
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-30 02:29:09 +0000

    / ‘Euler/arrays.hs’

--- Euler/arrays.hs
+++ Euler/arrays.hs
@@ -14,3 +14,8 @@ genCardToInt :: GenCard -> Int
 genCardToInt (GenCard r s) = lookup ! (r,s)
   where
     lookup = listArray ((Two,Clubs),(Ace,Spades)) [x|x<-[0..51]]
+
+fibs :: Int -> Array Int Int
+fibs n = a
+  where a = array (0,n) ([(0,1), (1,1)]
+              ++ [(i, a!(i-2) + a!(i-1)) | i<-[2..n]])
