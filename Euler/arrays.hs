cfc4ba3e27e7cdfb859826ce8f9e55fbd3b16d88
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-30 01:44:14 +0000

    / ‘Euler/arrays.hs’

--- Euler/arrays.hs
+++ Euler/arrays.hs
@@ -1,3 +1,11 @@
 import Data.Array
 
+data Rank = Two | Three | Four | Five | Six | Seven | Eight
+          | Nine | Ten | Jack | Queen | King | Ace
+          deriving (Eq, Ord, Show, Read, Enum, Ix)
 
+data Suit = Clubs | Diamonds | Hearts | Spades
+            deriving (Eq, Ord, Show, Read, Enum, Ix)
+
+data GenCard = GenCard Rank Suit
+               deriving (Eq, Ord, Show, Read)
