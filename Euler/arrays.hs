f77d49465fff756416a7a99d59ffafb5c23da5a6
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-12-31 12:09:31 +0000

    / ‘Euler/arrays.hs’

--- Euler/arrays.hs
+++ Euler/arrays.hs
@@ -19,3 +19,11 @@ fibs :: Int -> Array Int Int
 fibs n = a
   where a = array (0,n) ([(0,1), (1,1)]
               ++ [(i, a!(i-2) + a!(i-1)) | i<-[2..n]])
+
+wavefront :: Int -> Array (Int,Int) Int
+wavefront n = a where
+              a = array ((1,1),(n,n))
+                    ([((1,j),1) | j <- [1..n]] ++
+                     [((i,1),1) | i <- [2..n]] ++
+                     [((i,j), a!(i,j-1) + a!(i-1,j-1) + a!(i-1,j))
+                                | i <- [2..n], j <- [2..n]])
