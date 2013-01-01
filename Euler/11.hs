58b6f860fc51665bb9006a569f4e3a899943b9b6
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-01-01 23:17:57 +0000

    / ‘Euler/11.hs’

--- Euler/11.hs
+++ Euler/11.hs
@@ -47,48 +47,50 @@ main = do
   putStrLn (show a)
   putStrLn "-----------------"
   let products = (downProducts a) ++ (rightProducts a) ++ (downRightProducts a) ++ (upRightProducts a)
-  putStrLn "Max four in a row="
   putStrLn (show products)
+  putStrLn "-----------------"
+  putStrLn "Max four in a row="
+  putStrLn $ maximum products
 
+--D: 0,0 -> 19,16
 downProducts :: Array (Integer, Integer) Int -> [Int]
 downProducts a = 
-  [a!i1 * a!i2 * a!i3 * a!i4 | x <- [0..19], y <- [0..19]
+  [a!i1 * a!i2 * a!i3 * a!i4 | x <- [0..19], y <- [0..16]
                              , let i1=(y,x)
                              , let i2=(y+1,x)
                              , let i3=(y+2,x)
-                             , let i4=(y+3,x) , i4 <= ( snd $ bounds a)
+                             , let i4=(y+3,x)
                              ]
 
+--R: 0,0 -> 16,19
 rightProducts :: Array (Integer, Integer) Int -> [Int]
 rightProducts a = 
-  [a!i1 * a!i2 * a!i3 * a!i4 | x <- [0..19], y <- [0..19]
+  [a!i1 * a!i2 * a!i3 * a!i4 | x <- [0..16], y <- [0..19]
                              , let i1=(y,x)
                              , let i2=(y,x+1)
                              , let i3=(y,x+2)
-                             , let i4=(y,x+3), i4 <= ( snd $ bounds a)
+                             , let i4=(y,x+3)
                              ]
 
+--DR: 0,0 -> 16,16
 downRightProducts :: Array (Integer, Integer) Int -> [Int]
 downRightProducts a = 
-  [a!i1 * a!i2 * a!i3 * a!i4 | x <- [0..19], y <- [0..19]
+  [a!i1 * a!i2 * a!i3 * a!i4 | x <- [0..16], y <- [0..16]
                              , let i1=(y,x)
                              , let i2=(y+1,x+1)
                              , let i3=(y+2,x+2)
-                             , let i4=(y+3,x+3), i4 <= (snd $ bounds a)
+                             , let i4=(y+3,x+3)
                              ]
 
+--UR: 0,3 -> 16,19
 upRightProducts :: Array (Integer, Integer) Int -> [Int]
 upRightProducts a =
-  [a!i1 * a!i2 * a!i3 * a!i4 | x <- [0..19], y <- [3..19]
+  [a!i1 * a!i2 * a!i3 * a!i4 | x <- [0..16], y <- [3..19]
                              , let i1=(y,x)
                              , let i2=(y-1,x+1)
                              , let i3=(y-2,x+2)
-                             , let i4=(y-3,x+3), i4 >= (0,0)
+                             , let i4=(y-3,x+3)
                              ] 
 
 --Product of all fours in a row - y, x
 --Diagonal grid: 0,0 -> 19,19
---R: 0,0 -> 16,19
---D: 0,0 -> 19,16
---DR: 0,0 -> 16,16
---UR: 0,3 -> 16,19
