29bb3f2fedf05082ad1a12de08b9f6643c534827
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-01-02 00:51:34 +0000

    / ‘Euler/12.hs’

--- Euler/12.hs
+++ Euler/12.hs
@@ -2,4 +2,12 @@ upTo n (x:xs)
   | x > n = []
   | otherwise = x:(upTo n xs)
 
+triangular :: [Int]
 triangular = [foldr (+) 0 [1..n] | n <- [1..]]
+
+numberOfFactors :: Int -> Int
+numberOfFactors n = foldr (+) . isFactor 0 [1..n] 
+	where
+		isFactor f 
+			| n `mod` f == 0	= 1
+			| otherwise 		= 0
