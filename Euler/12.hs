acce7c405236960f08f9afb2ace674c26d05b053
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-01-06 20:37:26 +0000

    / ‘Euler/12.hs’

--- Euler/12.hs
+++ Euler/12.hs
@@ -23,3 +23,35 @@ numberOfFactors n = foldr ((+) . isFactor) 0 [1..n]
 			| n `mod` f == 0	= 1
 			| otherwise 		= 0
 
+memoizedIsPrime :: Int -> Bool
+memoizedIsPrime = (map isPrime [0..] !!)
+  where
+    isPrime :: Int -> Bool
+    isPrime n = isNotDivisibleBy 2
+      where
+         root_n = floor $ sqrt $ fromIntegral n
+         isNotDivisibleBy f 
+            | f > root_n = True
+            | n `mod` f == 0 = False
+            | otherwise = isNotDivisibleBy (f+1)
+
+primes = [n | n<-[2..], memoizedIsPrime n]
+
+facs :: Int -> Int
+facs n 
+	| n == 1 = 1
+	| n > 1 = 2 * fac' n 1 1
+	where
+		fac' :: Int -> Int -> Int -> Int
+		fac' n factors primeCount 
+			| ( primes!primeCount * primes!primeCount > n ) = factors 
+			| otherwise =
+				fac' n (factors * (power+1)) (primeCount+1)
+					where
+						cp = primes!primeCount
+						power = fac'' 0 cp n
+						fac'' pow pri nn |
+							( nn `mod` pri == 0 ) = fac'' (pow+1) (pri*cp) (nn/pri)
+							otherwise = pow
+
+
