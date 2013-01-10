8e3088964ca1a54b667c938b84bba0e4103434b0
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-01-10 16:52:19 +0000

    / ‘LHUG/Algebra/talk.hs’

--- LHUG/Algebra/talk.hs
+++ LHUG/Algebra/talk.hs
@@ -1,3 +1,22 @@
 {-# LANGUAGE EmptyDataDecls, TypeOperators #-}
 
 data Unit = Unit
+-- ()
+
+
+--Need TypeOperators pragma for the :+ below
+data a :+ b = AddL a | AddR b
+-- Either
+
+data a :* b = Mul a b
+-- (a,b)
+
+--Need EmptyDataDecls pragma for below
+data Void
+
+type Two = Unit :+ Unit
+-- Bool
+
+-- a->b <=> b^a
+--
+-- The derivative of a regular type is its type of One-Hole contexts
