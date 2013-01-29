7ee95ecca7d642ad5aff87a6ee3c138de0db6910
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-01-29 22:26:34 +0000

    + ‘happstack/TypedRoute.hs’

---
+++ happstack/TypedRoute.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Control.Monad (msum)
+import Data.Char (toLower)
+import Happstack.Server (FromReqURI(..), nullConf, simpleHTTP
+                        , ok, dir, path)
+
+data Subject = World | Haskell
+
+sayHello : Subject -> String
+sayHello World = "Hello, World!"
+sayHello Haskell = "Greetings, Haskell!"
+
+instance FromReqURI Subject where
+  fromReqURI sub =
+    case map toLower sub of
+      "haskell" -> Just Haskell
+      "world" -> Just World
+      _ -> Nothing
+
+main = IO ()
+main = simpleHTTP nullConf $ dir "hello"
+          $ path $ \subject -> ok $ (sayHello subject)
