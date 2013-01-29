ccc8345e875681162643efaeefac67cd91dd5652
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-01-29 11:01:39 +0000

    / ‘happstack/Route.hs’

--- happstack/Route.hs
+++ happstack/Route.hs
@@ -6,4 +6,6 @@ import Happstack.Server (nullConf, simpleHTTP, ok, dir)
 main :: IO ()
 main = simpleHTTP nullConf $ msum [ dir "hello" $ ok "Hello, World!"
                                   , dir "goodbye" $ ok "Goodbye, World!"
+                                  , dir "waxing" $ dir "moon" $ ok "WM!"
+                                  , dirs "crescent/moon" $ ok "CM!"
                                   ]
