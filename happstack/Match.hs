c2d0d40f9c700cc779377ff3dd5d53ad9db22b4d
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-01-29 11:29:15 +0000

    + ‘happstack/Match.hs’

---
+++ happstack/Match.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Control.Monad (msum)
+import Happstack.Server (nullConf, simpleHTTP, ok, dir, path)
+
+main :: IO ()
+main = simpleHTTP nullConf $ msum [ 
+            dir "hello" $ path $ \s -> ok $ "Hello, " ++ s
+                                  ]
+
