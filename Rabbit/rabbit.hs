a29af8e3ce2273c3d37e9241518cbf60d2babed8
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-02-02 01:00:35 +0000

    + ‘Rabbit/rabbit.hs’

---
+++ Rabbit/rabbit.hs
@@ -0,0 +1,30 @@
+import Network.AMQP
+import qualified Data.ByteString.Lazy.Char8 as BL
+
+main = do
+  conn <- openConnection "127.0.0.1" "/" "guest" "guest"
+  chan <- openChannel conn
+
+  -- declare a queue, exchange and binding
+  declareQueue chan newQueue {queueName = "myQueue"}
+  declareExchange chan newExchange {exchangeName = "myExchange", exchangeType = "direct"}
+  bindQueue chan "myQueue" "myExchange" "myKey"
+
+  -- subscribe to the queue
+  consumeMsgs chan "myQueue" Ack myCallback
+
+  -- publish a message to our new exchange
+  publishMsg chan "myExchange" "myKey" 
+    newMsg {msgBody = (BL.pack "hello world"), 
+            msgDeliveryMode = Just Persistent}
+
+  getLine -- wait for keypress
+  closeConnection conn
+  putStrLn "connection closed"
+
+                                                                    
+myCallback :: (Message,Envelope) -> IO ()
+myCallback (msg, env) = do
+  putStrLn $ "received message: "++(BL.unpack $ msgBody msg)
+  -- acknowledge receiving the message
+  ackEnv env
