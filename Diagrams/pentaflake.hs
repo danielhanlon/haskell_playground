1bc3834d4303517a5fde1ddf0a1c4cad707c1b89
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-03-15 22:53:16 +0000

    + ‘Diagrams/pentaflake.hs’

---
+++ Diagrams/pentaflake.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE NoMonomorphismRestrictions #-}
+
+import Diagrams.Prelude
+
+pentaflake' 0 = regPoly 5 1 # lw 0
+pentaflake' n = appends
+		 pCenter
+		 (zip vs (repeat (rotateBy (1/2) pOutside)))
+	where vs = iterateN 5 (rotateBy (1/5))
+		 . (if odd n then negateV else id)
+		 $ unitY
+	     pCenter = pentaflake' (n-1)
+	     pOutside = pCenter # opacity 0.8
+
+example = pad 1.1 $ pentaflake 4
