9a86309ff2ecb553a9f71d0992d5459482ee9387
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-06-18 14:58:11 +0200

    / ‘Diagrams/pentaflake.hs’

--- Diagrams/pentaflake.hs
+++ Diagrams/pentaflake.hs
@@ -2,12 +2,12 @@
 
 import Diagrams.Prelude
 
+main = defaultMain ( pad 1.1 $ pentaflake 4 )
+
 pentaflake' 0 = regPoly 5 1 # lw 0
 pentaflake' n = appends pCenter (zip vs (repeat (rotateBy (1/2) pOutside)))
-	where vs = iterateN 5 (rotateBy (1/5)) . (if odd n then negateV else id) $ unitY
-	      pCenter = pentaflake' (n-1)
-	      pOutside = pCenter # opacity 0.8
-
+  where vs = iterateN 5 (rotateBy (1/5)) . (if odd n then negateV else id) $ unitY
+    pCenter = pentaflake' (n-1)
+    pOutside = pCenter # opacity 0.8
 pentaflake n = pentaflake' n # fc blue # bg white
 
-main = defaultMain ( pad 1.1 $ pentaflake 4 )
