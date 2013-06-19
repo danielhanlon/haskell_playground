50dc8929c8fac37eb336cfcdddb4baca1200f148
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-06-19 11:36:06 +0200

    / ‘Diagrams/tut_isc.hs’

--- Diagrams/tut_isc.hs
+++ Diagrams/tut_isc.hs
@@ -3,12 +3,4 @@
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
-pentaflake' 0 = regPoly 5 1 # lw 0
-pentaflake' n = appends pCenter (zip vs (repeat (rotateBy (1/2) pOutside)))
-  where vs = iterateN 5 (rotateBy (1/5)) . (if odd n then negateV else id) $ unitY
-        pCenter = pentaflake' (n-1)
-        pOutside = pCenter # opacity 0.8
-
-pentaflake n = pentaflake' n # fc blue # bg white
-
-main = defaultMain ( pad 1.1 $ pentaflake 4 )
+main = defaultMain circle 1 # fc red # lw 0 ||| circle 1 # fc green # lw 0
