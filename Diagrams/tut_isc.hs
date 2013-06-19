58307471fe2e6042cb4ae99c6c2e48f511f4e94d
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-06-19 11:36:26 +0200

    / ‘Diagrams/tut_isc.hs’

--- Diagrams/tut_isc.hs
+++ Diagrams/tut_isc.hs
@@ -3,4 +3,4 @@
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
-main = defaultMain circle 1 # fc red # lw 0 ||| circle 1 # fc green # lw 0
+main = defaultMain $ circle 1 # fc red # lw 0 ||| circle 1 # fc green # lw 0
