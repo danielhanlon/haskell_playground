3474a3aeaabea1e0136857672f1fb96d765da4d7
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-06-19 15:00:48 +0200

    / ‘Diagrams/tut_isc.hs’

--- Diagrams/tut_isc.hs
+++ Diagrams/tut_isc.hs
@@ -4,4 +4,4 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
 pCircle1' = circle 1 # fc blue # lw 0.05 # lc purple # dashing [0.2,0.05] 0
-main = defaultMain $ pCircle1' # scale 0.3 `atop` pCircle1' `atop` square 1 # fc aqua 
+main = defaultMain $ pCircle1' # scale 0.3 |||` pCircle1' ===  square 1 # fc aqua 
