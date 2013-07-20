65cda4312a5cdba2936d15a454aa6cdf67e6742e
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-07-20 05:59:37 +0100

    / ‘caesar.hs’

--- caesar.hs
+++ caesar.hs
@@ -1,13 +1,13 @@
 import Data.Char (ord, chr)
 
 main = do
-  putChar caesar getChar
+  putChar $ caesar getChar
 
 caesar :: Int -> String -> String
 caesar shift plaintext = caesar' plaintext
   where
-    caesar' (c:cs) = [newC] ++ caesar' cs
-    caesar' (c:[]) = [newC]
-    c_chr = ord c
-    newC_chr = shift + c_chr `mod` 26
-    newC = chr newC_chr
+    enc c = [chr newC_chr]
+      where c_chr = ord c
+            newC_chr = shift + c_chr `mod` 26
+    caesar' (c:cs) = enc c ++ caesar' cs
+    caesar' (c:[]) = enc c
