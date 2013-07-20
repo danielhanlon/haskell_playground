4022c991208e75ff535bb78238115bd99903c3ac
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-07-20 19:28:14 +0100

    / ‘bintree.hs’

--- bintree.hs
+++ bintree.hs
@@ -5,9 +5,5 @@ add (Leaf i) = i
 add (Branch i t1 t2) = i + add t1 + add t2
 
 main = do
-  let sum = add $
-    Branch 4
-      (Leaf 2) (Branch 4
-                 (Branch 2
-                    Leaf 3 Leaf 2) Leaf 5)
+  let sum = add $ Branch 4 (Leaf 2) (Branch 4 (Branch 2 Leaf 3 Leaf 2) Leaf 5)
   putStrLn sum
