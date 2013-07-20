472d07b6234bc66064a718d4f1e95b5f09d615e9
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-07-20 06:25:41 +0100

    / ‘caesar.hs’

--- caesar.hs
+++ caesar.hs
@@ -8,7 +8,7 @@ caesar :: Int -> String -> String
 caesar shift plaintext = caesar' plaintext
   where
     enc c 
-      | c == ' '   = ' '
+      | c == ' '   = " "
       | otherwise  = [chr newC_chr]
       where c_chr = (ord $ toUpper c)-65
             newC_chr = 65 + ((shift + c_chr) `mod` 26)
