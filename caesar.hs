90b8976b863c777ea18a4f56503bceb5ead7bc60
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-07-20 06:17:30 +0100

    / ‘caesar.hs’

--- caesar.hs
+++ caesar.hs
@@ -8,7 +8,7 @@ caesar :: Int -> String -> String
 caesar shift plaintext = caesar' plaintext
   where
     enc c = [chr newC_chr]
-      where c_chr = ord . toUpper c
+      where c_chr = ord $ toUpper c
             newC_chr = shift + c_chr `mod` 65
     caesar' (c:[]) = enc c
     caesar' (c:cs) = enc c ++ caesar' cs
