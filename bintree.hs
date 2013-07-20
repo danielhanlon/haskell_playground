208a93408b99884826bca27e3c1b757436400bcf
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-07-20 19:39:58 +0100

    / ‘bintree.hs’

--- bintree.hs
+++ bintree.hs
@@ -7,3 +7,5 @@ add (Branch i t1 t2) = i + add t1 + add t2
 main = do
   let sum = add $ Branch 4 (Leaf 2) (Branch 4 (Branch 2 (Leaf 3) (Leaf 2)) (Leaf 5))
   putStrLn $ show sum
+
+data Point = Point { x::Double, y::Double}
