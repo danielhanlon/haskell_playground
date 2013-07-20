cd70006d3d3864f97a74e8998d034dc2dae7d45c
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-07-20 19:18:10 +0100

    / ‘bintree.hs’

--- bintree.hs
+++ bintree.hs
@@ -1,5 +1,5 @@
 data Tree = Leaf Int | Branch Int Tree Tree
 
 add :: Tree -> Int
-add Leaf i = i
-add Branch i t1 t2 = i + add t1 + add t2
+add (Leaf i) = i
+add (Branch i t1 t2) = i + add t1 + add t2
