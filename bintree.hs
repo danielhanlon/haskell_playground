8e523ef187a59e527bc9875fcd2e894dc8caff92
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-07-20 19:25:10 +0100

    / ‘bintree.hs’

--- bintree.hs
+++ bintree.hs
@@ -3,3 +3,11 @@ data Tree = Leaf Int | Branch Int Tree Tree
 add :: Tree -> Int
 add (Leaf i) = i
 add (Branch i t1 t2) = i + add t1 + add t2
+
+main = do
+  let sum = add 
+    Branch 4
+      (Leaf 2) (Branch 4
+                 (Branch 2
+                    Leaf 3 Leaf 2) Leaf 5)
+  putStrLn sum
