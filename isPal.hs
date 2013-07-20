8adbadc2c4b102a8e64b956f48305af2422a1d92
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-07-20 06:41:47 +0100

    + ‘isPal.hs’

---
+++ isPal.hs
@@ -0,0 +1,16 @@
+import Data.Char (ord, chr, toUpper)
+
+main = do
+  line <- getLine
+  putStrLn $ caesar 3 line
+
+caesar :: Int -> String -> String
+caesar shift plaintext = caesar' plaintext
+  where
+    enc c 
+      | c == ' '   = " "
+      | otherwise  = [chr newC_chr]
+      where c_chr    = (ord $ toUpper c)-65
+            newC_chr = 65 + ((shift + c_chr) `mod` 26)
+    caesar' []     = []
+    caesar' (c:cs) = enc c ++ caesar' cs
