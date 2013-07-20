2a6287a7efdfa9ea944307cbd5c73cd80a47433c
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-07-20 06:16:41 +0100

    / ‘caesar.hs’

--- caesar.hs
+++ caesar.hs
@@ -1,4 +1,4 @@
-import Data.Char (ord, chr)
+import Data.Char (ord, chr, toUpper)
 
 main = do
   line <- getLine
@@ -8,7 +8,7 @@ caesar :: Int -> String -> String
 caesar shift plaintext = caesar' plaintext
   where
     enc c = [chr newC_chr]
-      where c_chr = ord c
-            newC_chr = shift + c_chr `mod` 26
+      where c_chr = ord . toUpper c
+            newC_chr = shift + c_chr `mod` 65
     caesar' (c:[]) = enc c
     caesar' (c:cs) = enc c ++ caesar' cs
