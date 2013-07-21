10b43bb6b1e1918a3597ecc6125d0345bc4d0e0e
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-07-21 21:39:33 +0100

    / ‘password_check.hs’

--- password_check.hs
+++ password_check.hs
@@ -1,8 +1,13 @@
+import Data.Char (isUpper, isLower, isNumber)
 
 check :: String -> Bool
-check pw = undefined
-  where fifteen_chars = 
-
+check pw = fifteen_chars && uppercase && lowercase && number
+  where
+    fifteen_chars = length pw >= 15
+    uppercase = any (\c -> isUpper c) pw
+    lowercase = any (\c -> isLower c) pw
+    number = any (\c -> isNumber c) pw
+  
 main = do 
   pw <- getLine
   case check pw of
