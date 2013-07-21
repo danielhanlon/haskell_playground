66d058814600a53e38ef9e0096789a2b38ca5780
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-07-21 21:32:50 +0100

    / ‘password_check.hs’

--- password_check.hs
+++ password_check.hs
@@ -1,8 +1,9 @@
+check :: String -> Bool
+check pw = undefined
+
 main = do 
   pw <- readLn
   case check pw of
     True   -> putStrLn "Good"
     False  -> putStrLn "Bad"
   
- check :: String -> Bool
- check pw = undefined
