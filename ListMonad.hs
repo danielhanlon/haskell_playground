840da3c7acf674b73ba99a1ad24dca5fb6dc4a6f
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-09-18 13:43:02 +0100

    / ‘ListMonad.hs’

--- ListMonad.hs
+++ ListMonad.hs
@@ -1,7 +1,9 @@
+data MyResult = MyResult (Int,Int) deriving Show
+
 getTuples = do 
     x <- [1..10]
     y <- [2..11]
-    return (x,y)
+    return MyResult (x,y)
 
 main = do
     mapM putStrLn getTuples
