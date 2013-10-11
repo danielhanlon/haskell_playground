d9122ba97b934adeb5c8a1394fd76f2fe18165c2
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-10-11 22:11:03 +0100

    + ‘Yesod/Tutorial/ex1.hs’

---
+++ Yesod/Tutorial/ex1.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses,
+  -              TemplateHaskell, OverloadedStrings #-}
+import Yesod
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler RepHtml
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
+main :: IO ()
+main = warpDebug 3000 HelloWorld
