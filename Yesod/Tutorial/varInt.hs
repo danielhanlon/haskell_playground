dd497614ee966c1ba89480beadfe1e755196f352
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-10-13 21:52:16 +0100

    + ‘Yesod/Tutorial/varInt.hs’

---
+++ Yesod/Tutorial/varInt.hs
@@ -0,0 +1,21 @@
+{# LANGUAGE QuasiQuotes #-}
+import Text.Hamlet (shamlet)
+import Text.Blaze.Renderer.String(renderHtml)
+import Data.Char (toLower)
+import Data.List (sort)
+
+data Person = Person
+  { name :: String
+  , age  :: Int
+  }
+
+main :: IO()
+main = putStrLn $ renderHtml [shamlet|
+<p>Hello, my name is #{name person} and I am #{show $ age person}.
+<p>
+  Let's do some funny stuff with my name: #
+  <b>#{sort $ map toLower (name person)}
+<p>Oh, and in 5 years I'll be #{show (5 + (age person))} years old.
+|]
+  where
+    person = Person "Daniel" 39
