3cd222ca98cd89ee2772623ec900fdf0ee48c821
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-10-13 22:01:13 +0100

    + ‘Yesod/Tutorial/typeSafeURLs.hs’

---
+++ Yesod/Tutorial/typeSafeURLs.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE QuasiQuotes,OverloadedString #-}
+
+import Text.Hamlet (HtmlUrl, hamlet)
+import Text.Blaze.Renderer.String (renderHtml)
+import Data.Text (Text)
+
+data MyRoute = Home
+
+render :: MyRoute -> [(Text,Text)] -> Text
+render Home _ = "/home"
+
+footer :: HtmlUrl MyRoute
+footer = [hamlet|
+<footer>
+  Return to #
+  <a href=@{Home}>Homepage
+  .
+|]
+
+main :: IO ()
+main = putStrLn $ renderHtml $ [hamlet|
+<body>
+  <p>This is my page.
+  ^{footer}
+|] render
