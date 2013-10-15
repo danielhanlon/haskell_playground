d817e67c3c7aa5e32f174d60ad515212362d2ef2
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-10-15 12:05:34 +0100

    - ‘1581195.mp4?token2=1381826788_8972fa64ea3e94327737da897acca8ea’ - ‘esod/CharlieHarvey/SnippIO/.ghci’ - ‘esod/CharlieHarvey/SnippIO/.gitignore’ - ‘esod/CharlieHarvey/SnippIO/Application.hs’ - ‘esod/CharlieHarvey/SnippIO/Foundation.hs’ - ‘esod/CharlieHarvey/SnippIO/Handler/Home.hs’ - ‘esod/CharlieHarvey/SnippIO/Import.hs’ - ‘esod/CharlieHarvey/SnippIO/Model.hs’ - ‘esod/CharlieHarvey/SnippIO/Settings.hs’ - ‘esod/CharlieHarvey/SnippIO/Settings/Development.hs’ ...

---
+++ Yesod/CharlieHarvey/demo/Handler/Home.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE TupleSections, OverloadedStrings #-}
+module Handler.Home where
+
+import Import
+
+-- This is a handler function for the GET request method on the HomeR
+-- resource pattern. All of your resource patterns are defined in
+-- config/routes
+--
+-- The majority of the code you will write in Yesod lives in these handler
+-- functions. You can spread them across multiple files if you are so
+-- inclined, or create a single monolithic file.
+getHomeR :: Handler Html
+getHomeR = do
+    (formWidget, formEnctype) <- generateFormPost sampleForm
+    let submission = Nothing :: Maybe (FileInfo, Text)
+        handlerName = "getHomeR" :: Text
+    defaultLayout $ do
+        aDomId <- newIdent
+        setTitle "Welcome To Yesod!"
+        $(widgetFile "homepage")
+
+postHomeR :: Handler Html
+postHomeR = do
+    ((result, formWidget), formEnctype) <- runFormPost sampleForm
+    let handlerName = "postHomeR" :: Text
+        submission = case result of
+            FormSuccess res -> Just res
+            _ -> Nothing
+
+    defaultLayout $ do
+        aDomId <- newIdent
+        setTitle "Welcome To Yesod!"
+        $(widgetFile "homepage")
+
+sampleForm :: Form (FileInfo, Text)
+sampleForm = renderDivs $ (,)
+    <$> fileAFormReq "Choose a file"
+    <*> areq textField "What's on the file?" Nothing
+
+noteForm :: Form Note
+noteForm = renderBootstrap $ Note
+    <$> areq textField "Title" Nothing
+    <*> areq textField "Content" Nothing
+
+getNotesR = do
+    (formWidget, enctype) <- generateFormPost noteForm
+    defaultLayout $(widgetFile "notes")
+
+postNotesR = return ()
+
+getNoteR noteid = return ()
