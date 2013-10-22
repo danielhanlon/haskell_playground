91aec536a63e2fee4213ac2f4431b67e6ba5e811
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-10-22 19:11:12 +0100

    / ‘Yesod/FPComplete/FPComplete-tutorial/Application.hs’ / ‘Yesod/FPComplete/FPComplete-tutorial/FPComplete-tutorial.cabal’ + ‘Yesod/FPComplete/FPComplete-tutorial/Handler/Reverse.hs’ / ‘Yesod/FPComplete/FPComplete-tutorial/config/routes’

---
+++ Yesod/FPComplete/FPComplete-tutorial/Handler/Reverse.hs
@@ -0,0 +1,9 @@
+module Handler.Reverse where
+
+import Import
+
+getReverseR :: text -> Handler Html
+getReverseR = error "Not yet implemented: getReverseR"
+
+postReverseR :: text -> Handler Html
+postReverseR = error "Not yet implemented: postReverseR"
