beb0f6815e6fde92d61ea55dfd35dbb834995c5b
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-10-22 19:13:27 +0100

    / ‘Yesod/FPComplete/FPComplete-tutorial/Handler/Reverse.hs’

--- Yesod/FPComplete/FPComplete-tutorial/Handler/Reverse.hs
+++ Yesod/FPComplete/FPComplete-tutorial/Handler/Reverse.hs
@@ -4,6 +4,3 @@ import Import
 
 getReverseR :: text -> Handler Html
 getReverseR = error "Not yet implemented: getReverseR"
-
-postReverseR :: text -> Handler Html
-postReverseR = error "Not yet implemented: postReverseR"
