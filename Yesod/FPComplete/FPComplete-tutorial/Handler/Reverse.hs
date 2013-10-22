4e8c446cda2836ab9c0e7ed12e6eddb2dec85097
Daniel Hanlon <d.hanlon@ucl.ac.uk>
2013-10-22 19:26:06 +0100

    / ‘Yesod/FPComplete/FPComplete-tutorial/Handler/Reverse.hs’

--- Yesod/FPComplete/FPComplete-tutorial/Handler/Reverse.hs
+++ Yesod/FPComplete/FPComplete-tutorial/Handler/Reverse.hs
@@ -3,6 +3,6 @@ module Handler.Reverse where
 import Import
 import Data.List (reverse)
 
-getReverseR :: String -> Handler Html
+getReverseR :: Text -> Handler Html
 getReverseR theText =
     defaultLayout $ [whamlet|<h1>The text reversed is:#{reverse theText}|]
