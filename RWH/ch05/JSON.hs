b4e1dadabfce4051e57b285dfae83dee260727d9
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2013-11-19 23:54:16 +0000

    + ‘RWH/ch05/JSON.hs’

---
+++ RWH/ch05/JSON.hs
@@ -0,0 +1,7 @@
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String,JValue)]
+            | JArray [JValue]
+              deriving (Eq, Ord, Show)
